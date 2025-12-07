75821e07e06d242214783710fc2dd6534549af4b
ArturErnandes <al211205@yandex.ru>
2025-12-07 14:00:16 +0300

    Фронт первой страницы + общая настройка формата репозитория

---
+++ app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from telebot import TeleBot, types
+import uvicorn
+import threading
+
+BOT_TOKEN = "8506035979:AAE6H3ta1v8uObASeHbICd2TdU2Wd0SXC4g"
+
+# ВАЖНО: позже сюда вставишь ngrok URL
+WEBAPP_URL = "file:///Users/arturernandeslitvinov/PycharmProjects/ABN/Channels_market/web/index.html#"
+
+bot = TeleBot(BOT_TOKEN, parse_mode="HTML")
+app = FastAPI()
+
+# Раздаём папку web как статику
+app.mount("/web", StaticFiles(directory="web"), name="web")
+
+@app.get("/")
+def serve_page():
+    return FileResponse("web/index.html")
+
+# БОТ
+@bot.message_handler(commands=['start'])
+def start(message):
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = types.KeyboardButton(
+        text="Открыть мини-приложение",
+        web_app=types.WebAppInfo(url=WEBAPP_URL)
+    )
+    kb.add(btn)
+
+    bot.send_message(
+        message.chat.id,
+        "Нажми кнопку, чтобы открыть мини-приложение.",
+        reply_markup=kb
+    )
+
+@bot.message_handler(content_types=['web_app_data'])
+def process_webapp(message):
+    data = message.web_app_data.data
+    bot.send_message(message.chat.id, f"Получил данные:\n<code>{data}</code>")
+
+# Запуск FastAPI в отдельном потоке
+def run_server():
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_server, daemon=True).start()
+    bot.infinity_polling(skip_pending=True)
